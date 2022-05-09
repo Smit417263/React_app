a4112b586ee069402ac21abeaea58a3812feda95
Azmahds <azmahds@gmail.com>
2022-05-09 16:36:46 -0700

    added like and validation

--- my-app/src/Comment.js
+++ my-app/src/Comment.js
@@ -6,18 +6,30 @@ function Comment(){
     const [name, setName] = useState(document.getElementById('name').value);
     const [body, setBody] = useState(document.getElementById('body-text').value)
     const [show, setShow] = useState(false)
+    const [cnt, setCnt] = useState(0);
     const [replies, setReplies] = useState([])
 
     return (
         <React.Fragment>
-            <p>{name}</p>
-            <p>{body}</p>
+            <div>
+                <p>{name}</p>
+                <p>{body}</p>
+                
+                <button onClick={() => setCnt(cnt + 1)}>^</button>
+                <br />
+                <a>{cnt}</a>
+                <br />
+                <button onClick={() => setCnt(cnt - 1)}>v</button>
+            </div>
+            <br/>
+            <br/>
             <button onClick={() => setShow(true)}>Reply</button>
             <Modal onClose={() => setShow(false)} onSubmit={() => {
                 var reply = <Reply />;
                 setReplies([...replies, reply]);
                 console.log("got here")
             }}show={show}/>
+    
             {replies}
         </React.Fragment>
     )
--- my-app/src/Reply.js
+++ my-app/src/Reply.js
@@ -5,12 +5,20 @@ function Reply(){
     const [name, setName] = useState(document.getElementById('mname'));
     const [body, setBody] = useState(document.getElementById('mbody-text'))
     const [show, setShow] = useState(false)
+    const [cnt, setCnt] = useState(0);
     const [replies, setReplies] = useState([])
-    console.log(name, body);
     return (
         <React.Fragment>
             <p>{name.value}</p>
             <p>{body.value}</p>
+
+            <button onClick={() => setCnt(cnt + 1)}>^</button>
+            <br />
+            <a>{cnt}</a>
+            <br />
+            <button onClick={() => setCnt(cnt - 1)}>v</button>
+            <br />
+            <br />
             <button onClick={() => setShow(true)}>Reply</button>
             <Modal onClose={() => setShow(false)} onSubmit={(newReply) => {
                 setReplies([...replies, newReply])
