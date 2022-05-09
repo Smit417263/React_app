73bf363458eb51f737f33308e7fda1f891a528d7
Azmahds <azmahds@gmail.com>
2022-05-09 14:31:56 -0700

    initial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Reply from './Reply.js'
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    var comment = <Reply name={document.getElementById('name').value} body={document.getElementById('body-text').value}/>
+    console.log(comment)
+    setData(data);
+  })
+
+
   return (
+    <React.Fragment>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='modal' id='modal'>
+            <div className='modal-content'>
+                <div className='modal-header'>
+                    <h1>New Post</h1>
+                </div>
+                <div className='modal-body'>
+                    <input placeholder='Name' id="name"></input>
+                    <br/>
+                    <input placeholder='Write a new post' id="body-text"></input>
+                </div>
+                <div className='modal-footer'>
+                    <button onClick={() => setData()}>submit</button>
+                </div>
+            </div>
+        </div>
     </div>
+    {data}
+    </React.Fragment>
   );
 }
 
